08552bd775f1ea6c8b86842a6773fb644520a42b
Temitope <temmieayodele@gmail.com>
2019-06-01 11:18:18 +0100

    more edits

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const showNav = document.querySelector("#show-nav");
 const list = document.querySelector("#list");
 const navBar = document.querySelector("#nav-bar");
+// const uli = document.querySelector("ul");
 
-console.log(showNav)
-showNav.addEventListener('click', ()=>{
-    // showNav.style.display = 'none';
-    console.log("clicked");
-    list.style.display = 'block';
-})
+console.log(showNav);
+showNav.addEventListener("click", () => {
+  console.log("clicked");
+  if (list.style.display == "block") {
+    list.style.display = "none";
+    console.log("block");
+  } else {
+    list.style.display = "block";
+  }
+});
