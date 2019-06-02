cd7fbe2bb2c439739732967365b435ab9c8c4dd3
Temitope <temmieayodele@gmail.com>
2019-06-02 11:59:02 +0100

    more edits

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const showNav = document.querySelector("#show-nav");
-const list = document.querySelector("#list");
-const navBar = document.querySelector("#nav-bar");
-// const uli = document.querySelector("ul");
 
-console.log(showNav);
-showNav.addEventListener("click", () => {
-  console.log("clicked");
-  if (list.style.display == "block") {
-    list.style.display = "none";
-    console.log("block");
-  } else {
-    list.style.display = "block";
-  }
-});
